9feb2f05821494d02ef67c8cff686508663490da
olnbxxx <117682412+olnbxxx@users.noreply.github.com>
2022-12-21 22:11:08 +0100

    ejercicios de objetos

---
+++ objetos.js
@@ -0,0 +1,21 @@
+const mySelf = {
+    nombre: "Oscar",
+    apellido: "Nicolas",
+    edad: 45,
+    altura: 1.70,
+    eresDesarrollador: true
+}
+let old = mySelf.edad;
+
+const lista = [mySelf];
+const amigos = {
+    
+        friend1: "urtiz",
+        edadFriend1: 35,
+    
+        friend2: "jose",
+        edadFriend2: 47,
+}
+const lista2 = [mySelf, amigos];
+
+console.log(lista2);
